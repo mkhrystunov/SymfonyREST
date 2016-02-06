8b1584a2f1a921ad4498a0572c8159f5f90b8c73
Maksym Khrystunov <khristunov93@gmail.com>
2016-02-06 15:42:53 +0200

    Added Photos

---
+++ src/AppBundle/Form/PhotoType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PhotoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file', 'file', [
+                'required' => false,
+            ])
+            ->add('tags', 'collection', [
+                'type' => 'text',
+                'allow_add' => true,
+                'allow_delete' => true,
+                'delete_empty' => true,
+                'options' => [
+                    'required' => false,
+                ],
+            ]);
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Photo',
+            'csrf_protection' => false,
+        ]);
+    }
+
+    /**
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'photo';
+    }
+}
