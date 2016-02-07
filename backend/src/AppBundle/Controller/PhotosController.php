e69bef460896bba7fad4c02bb04ea47f0eee259d
Maksym Khrystunov <khristunov93@gmail.com>
2016-02-07 10:40:03 +0200

    Move symfony part to backend folder

---
+++ backend/src/AppBundle/Controller/PhotosController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Photo;
+use AppBundle\Form\PhotoType;
+use FOS\RestBundle\Controller\Annotations\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PhotosController extends Controller
+{
+    /**
+     * @return array
+     * @View
+     */
+    public function getPhotosAction()
+    {
+        $photos = $this->getDoctrine()->getRepository('AppBundle:Photo')->findAll();
+
+        return ['photos' => $photos];
+    }
+
+    /**
+     * @param Photo $photo
+     * @return array
+     * @View()
+     * @ParamConverter("photo", class="AppBundle:Photo")
+     */
+    public function getPhotoAction(Photo $photo)
+    {
+        return ['photo' => $photo];
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function postPhotosAction(Request $request)
+    {
+        $photo = new Photo();
+        $form = $this->createForm(new PhotoType(), $photo);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $file = $photo->getFile();
+
+            $fileName = sprintf('%s.%s', md5(uniqid()), $file->guessExtension());
+
+            $photosDir = $this->container->getParameter('kernel.root_dir') . '/../web/uploads/photos';
+            $file->move($photosDir, $fileName);
+
+            $photo->setFileName($fileName);
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($photo);
+            $manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_CREATED);
+        }
+        return new JsonResponse((string)$form->getErrors(), Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @param Photo $photo
+     *
+     * @param Request $request
+     * @ParamConverter("photo", class="AppBundle:Photo")
+     * @return JsonResponse|array
+     * @View
+     */
+    public function putPhotosAction(Photo $photo, Request $request)
+    {
+        $form = $this->createForm(new PhotoType(), $photo, [
+            'method' => 'PUT',
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($photo);
+            $manager->flush();
+            return ['photo' => $photo];
+        }
+        return new JsonResponse((string)$form->getErrors(), Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @param Photo $photo
+     *
+     * @ParamConverter("photo", class="AppBundle:Photo")
+     * @return JsonResponse
+     */
+    public function deletePhotosAction(Photo $photo)
+    {
+        try {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($photo);
+            $manager->flush();
+            return new JsonResponse('success', Response::HTTP_NO_CONTENT);
+        } catch (\Exception $ignored) {
+            return new JsonResponse('failed', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ backend/src/AppBundle/Entity/Photo.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="photos")
+ * @ORM\Entity
+ */
+class Photo
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var UploadedFile
+     *
+     * @Assert\Image()
+     */
+    private $file;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $fileName;
+
+    /**
+     * @var string[]
+     *
+     * @ORM\Column(type="array")
+     */
+    private $tags;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param UploadedFile|null $file
+     * @return $this
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @param string $fileName
+     * @return $this
+     */
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param string[] $tags
+     * @return $this
+     */
+    public function setTags(array$tags)
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+}
